02f43fd12e78224cb6550a629268118190261c33
Levent Durdalı <levent07@live.com>
2021-03-30 20:04:33 +0300

    Update DHT11-Lamp.ino

--- Project Code/DHT11-Lamp/DHT11-Lamp.ino
+++ Project Code/DHT11-Lamp/DHT11-Lamp.ino
@@ -1,8 +1,8 @@
 /*****
- 
- All the resources for this project:
- http://randomnerdtutorials.com/
- 
+
+  All the resources for this project:
+  http://randomnerdtutorials.com/
+
 *****/
 
 #include <ESP8266WiFi.h>
@@ -15,11 +15,11 @@
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 // Change the credentials below, so your ESP8266 connects to your router
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "---";
+const char* password = "---";
 
 // Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
-const char* mqtt_server = "REPLACE_WITH_YOUR_RPI_IP_ADDRESS";
+const char* mqtt_server = "192.168.1.65";
 
 // Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
 WiFiClient espClient;
@@ -56,14 +56,14 @@ void setup_wifi() {
 }
 
 // This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
-// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that
 // your ESP8266 is subscribed you can actually do something
 void callback(String topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
   String messageTemp;
-  
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
@@ -73,40 +73,40 @@ void callback(String topic, byte* message, unsigned int length) {
   // Feel free to add more if statements to control more GPIOs with MQTT
 
   // If a message is received on the topic room/lamp, you check if the message is either on or off. Turns the lamp GPIO according to the message
-  if(topic=="room/lamp"){
-      Serial.print("Changing Room lamp to ");
-      if(messageTemp == "on"){
-        digitalWrite(lamp, HIGH);
-        Serial.print("On");
-      }
-      else if(messageTemp == "off"){
-        digitalWrite(lamp, LOW);
-        Serial.print("Off");
-      }
+  if (topic == "room/lamp") {
+    Serial.print("Changing Room lamp to ");
+    if (messageTemp == "on") {
+      digitalWrite(lamp, HIGH);
+      Serial.print("On");
+    }
+    else if (messageTemp == "off") {
+      digitalWrite(lamp, LOW);
+      Serial.print("Off");
+    }
   }
   Serial.println();
 }
 
 // This functions reconnects your ESP8266 to your MQTT broker
-// Change the function below if you want to subscribe to more topics with your ESP8266 
+// Change the function below if you want to subscribe to more topics with your ESP8266
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
     /*
-     YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
-     To change the ESP device ID, you will have to give a new name to the ESP8266.
-     Here's how it looks:
+      YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
+      To change the ESP device ID, you will have to give a new name to the ESP8266.
+      Here's how it looks:
        if (client.connect("ESP8266Client")) {
-     You can do it like this:
+      You can do it like this:
        if (client.connect("ESP1_Office")) {
-     Then, for the other ESP:
+      Then, for the other ESP:
        if (client.connect("ESP2_Garage")) {
       That should solve your MQTT multiple connections problem
     */
     if (client.connect("ESP8266Client")) {
-      Serial.println("connected");  
+      Serial.println("connected");
       // Subscribe or resubscribe to a topic
       // You can subscribe to more topics (to control more LEDs in this example)
       client.subscribe("room/lamp");
@@ -125,9 +125,9 @@ void reconnect() {
 // The callback function is what receives messages and actually controls the LEDs
 void setup() {
   pinMode(lamp, OUTPUT);
-  
+
   dht.begin();
-  
+
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -141,12 +141,12 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  if(!client.loop())
+  if (!client.loop())
     client.connect("ESP8266Client");
 
   now = millis();
   // Publishes new temperature and humidity every 30 seconds
-  if (now - lastMeasure > 30000) {
+  if (now - lastMeasure > 10000) {
     lastMeasure = now;
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h = dht.readHumidity();
@@ -165,19 +165,19 @@ void loop() {
     float hic = dht.computeHeatIndex(t, h, false);
     static char temperatureTemp[7];
     dtostrf(hic, 6, 2, temperatureTemp);
-    
-    // Uncomment to compute temperature values in Fahrenheit 
+
+    // Uncomment to compute temperature values in Fahrenheit
     // float hif = dht.computeHeatIndex(f, h);
     // static char temperatureTemp[7];
     // dtostrf(hif, 6, 2, temperatureTemp);
-    
+
     static char humidityTemp[7];
     dtostrf(h, 6, 2, humidityTemp);
 
     // Publishes Temperature and Humidity values
     client.publish("room/temperature", temperatureTemp);
     client.publish("room/humidity", humidityTemp);
-    
+
     Serial.print("Humidity: ");
     Serial.print(h);
     Serial.print(" %\t Temperature: ");
@@ -190,4 +190,4 @@ void loop() {
     // Serial.print(hif);
     // Serial.println(" *F");
   }
-} 
+}
